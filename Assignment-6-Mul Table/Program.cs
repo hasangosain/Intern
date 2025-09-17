013609f2b8fd040bd7152ec60980221924430206
Hasan Gosain <hasangosain50@gmail.com>
2025-09-17 22:20:21 +0545

    Multiplication Table from 1 to 10 using nested loop.

---
+++ Assignment-6-Mul Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MultiplicationTables
+{
+    static void Main()
+    {
+        Console.WriteLine("Multiplication Tables from 1 to 10: \n");
+
+        for (int i = 1; i <= 10; i++)  // Loop for tables 1 to 10
+        {
+            Console.WriteLine($" Table of {i}: ");
+
+            for (int j = 1; j <= 10; j++)  // Loop for each multiplication row
+            {
+                Console.WriteLine($"{i} x {j} = {i * j}");
+            }
+
+            Console.WriteLine(); // Blank line between tables
+        }
+    }
+}
