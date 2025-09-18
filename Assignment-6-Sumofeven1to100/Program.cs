bab53e122a83f966a4233448e1b6b1ab9b5b3448
Hasan Gosain <hasangosain50@gmail.com>
2025-09-18 10:56:14 +0545

    Created a project that is sum of all even number from 1 to 100.

---
+++ Assignment-6-Sumofeven1to100/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sum of Even Numbers Between 1 and 100: ");
+
+        int sumEvenNum = CalculateEvenSum(1, 100);
+
+        Console.WriteLine($"The sum of all even numbers between 1 and 100 is: {sumEvenNum}");
+    }
+
+    // Method to calculate sum of even numbers in a given range
+    static int CalculateEvenSum(int firstNum, int lastNum)
+    {
+        int sumEvenNum = 0;
+
+        for (int i = firstNum; i <= lastNum; i++)
+        {
+            if (i % 2 == 0)  // check if number is even
+            {
+                sumEvenNum += i;
+            }
+        }
+
+        return sumEvenNum;
+    }
+}
