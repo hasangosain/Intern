84571816f5f729879b93073ae5b6767a21a7ace4
Hasan Gosain <hasangosain50@gmail.com>
2025-10-13 14:58:07 +0545

    Custom Exception Handling

--- Customs Exceptional Handling/Program.cs
+++ Customs Exceptional Handling/Program.cs
@@ -1,29 +1,27 @@
 ﻿
-//Slide 1: Definition
+// 1: Definition
 //Custom Exception Handling is the process of creating user-defined exception classes to handle 
 //specific errors in an application that are not adequately addressed by built-in exceptions.
 //User creates exception as Per the need of the code.
+//Also called User Defined Exception.
 
 //Example:
-//Program where user is less than 18 years age.
+//Program where user age is less than 18 years (not valid for vote).
 //In a banking system, withdrawing more than the available balance can throw an InsufficientFundsException.
 
-//Slide 2: When to Use Custom Exceptions
-//Use custom exceptions when:
-//Specific Domain Rules:
-//Errors are specific to your application, e.g., bank, library, or game logic.
+// 2: When to Use Custom Exceptions
 //Built-in Exceptions Are Too Generic:
 //Built -in exceptions like Exception or InvalidOperationException don’t convey enough information.
 //Need Meaningful Error Messages:
 
-//Slide 4: Merits
+// 4: Merits
 //Makes code self-explanatory and easier to maintain.
 //Enables precise and meaningful error handling.
 //Improves robustness and fault tolerance.
 //Helps in logging domain-specific errors effectively.
 
 
-//Slide 5: Demerits
+// 5: Demerits
 //Slightly increases code complexity.
 //May overuse exceptions, which can affect performance.
 //Requires extra effort to define and maintain multiple custom exceptions.
@@ -78,8 +76,4 @@ class Amnil
 
 
 
-
-//Advantages: clarity, maintainability, robustness.
-
-//Disadvantages: adds complexity, slight performance overhead.
 
